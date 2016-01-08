d4c73216160a1bc7c94b7687fdd08290ca56f2c9
HoraceVelmont <dragond7@naver.com>
2016-01-08 19:32:10 +0900

    T5 레이어 화면 분할 완료, 사이즈 문제 발생

--- VESS/Classes/AppDelegate.cpp
+++ VESS/Classes/AppDelegate.cpp
@@ -3,10 +3,10 @@
 
 USING_NS_CC;
 
-static cocos2d::Size designResolutionSize = cocos2d::Size(480, 320);
-static cocos2d::Size smallResolutionSize = cocos2d::Size(480, 320);
-static cocos2d::Size mediumResolutionSize = cocos2d::Size(1024, 768);
-static cocos2d::Size largeResolutionSize = cocos2d::Size(2048, 1536);
+static cocos2d::Size designResolutionSize = cocos2d::Size(960, 540);
+static cocos2d::Size smallResolutionSize = cocos2d::Size(1920, 1080);
+static cocos2d::Size mediumResolutionSize = cocos2d::Size(960, 540);
+static cocos2d::Size largeResolutionSize = cocos2d::Size(1920, 1080);
 
 AppDelegate::AppDelegate() {
 
---
+++ VESS/Classes/FightLayer.cpp
@@ -0,0 +1,23 @@
+#pragma once
+#include "FightLayer.h"
+
+bool FightLayer::init()
+{
+	if (!Layer::init()) {
+		return false;
+	}
+
+	Size visibleSize = Director::getInstance()->getVisibleSize();
+	Vec2 origin = Director::getInstance()->getVisibleOrigin();
+
+
+	// 전투 샘플 이미지
+	auto sprite = Sprite::create("Images/daughter.png");
+
+	// position the sprite on the center of the screen
+	sprite->setPosition(Vec2(visibleSize.width / 2 + origin.x, visibleSize.height / 2 + origin.y));
+	sprite->setScale(0.5f);
+
+	// add the sprite as a child to this layer
+	this->addChild(sprite, 0);
+}
