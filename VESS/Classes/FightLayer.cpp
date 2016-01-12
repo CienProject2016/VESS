d7469ace8ef6634d1d6e60f9eee0fdbc937d297b
HoraceVelmont <dragond7@naver.com>
2016-01-12 19:23:10 +0900

    T16 Add Movement into Monster Model

--- VESS/Classes/FightLayer.cpp
+++ VESS/Classes/FightLayer.cpp
@@ -1,12 +1,12 @@
 #pragma once
 #include "FightLayer.h"
+#include "Monster.h"
 
 bool FightLayer::init()
 {
 	if (!Layer::init()) {
 		return false;
 	}
-
 	Size visibleSize = Director::getInstance()->getVisibleSize();
 	log("fightLayer visibleWidthSize : %f", visibleSize.width);
 	log("fightLayer visibleHeightSize : %f", visibleSize.height);
@@ -16,29 +16,13 @@ bool FightLayer::init()
 
 	// 전투 샘플 이미지
 	auto daughterImage = Sprite::create("Images/daughter.png");
-	auto enemyImage = Sprite::create("Images/slime.png");
-
 	// position the sprite on the center of the screen	
 	daughterImage->setPosition(Vec2(origin.x, origin.y + visibleSize.height*0.5f));
 	daughterImage->setScale(1.0f);
 	//daughterImage->setAnchorPoint(ccp(0, 0.5f));
-	
-	enemyImage->setPosition(Vec2(origin.x + visibleSize.width*0.5f, origin.y + visibleSize.height*0.5f));
-	enemyImage->setScale(4.0f);
-	enemyImage->setVisible(true);
 
+	Monster* enemyImage = Monster::create("slime");
 	// add the sprite as a child to this layer
 	this->addChild(daughterImage, 0);
 	this->addChild(enemyImage, 1);
-
-	
-	auto enemyMove = MoveTo::create(4, ccp(origin.x, origin.y + visibleSize.height*0.5f));
-	auto enemyRelocate = MoveTo::create(0, ccp(origin.x + visibleSize.width*0.5f, origin.y + visibleSize.height*0.5f));
-
-	auto seq = CCSequence::create(enemyMove, CCDelayTime::create(2), enemyRelocate, CCDelayTime::create(2), NULL);
-	auto rep = CCRepeatForever::create(seq); //무한반복
-	enemyImage->runAction(rep);
-
-	
-	
 }
--- VESS/Classes/Unit.h
+++ VESS/Classes/Unit.h
@@ -1,11 +1,12 @@
 #ifndef __UNIT_H__
 #define __UNIT_H__
 
-class Unit {
+#include "cocos2d.h"
+
+USING_NS_CC;
+
+class Unit: public Node{
 protected:
-	int hp;
-public:
-	virtual int getHP();
-	virtual void setHP(int);
+	CC_SYNTHESIZE(int, hp_, HP);
 };
 #endif
