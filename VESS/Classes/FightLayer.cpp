94180e30b92d9b554d965fce58e8368c3e907bef
SoMinHyung <smhtop@naver.com>
2016-01-12 16:30:28 +0900

    T16_Monster_Move

--- VESS/Classes/FightLayer.cpp
+++ VESS/Classes/FightLayer.cpp
@@ -16,11 +16,29 @@ bool FightLayer::init()
 
 	// 전투 샘플 이미지
 	auto daughterImage = Sprite::create("Images/daughter.png");
+	auto enemyImage = Sprite::create("Images/slime.png");
 
 	// position the sprite on the center of the screen	
-	daughterImage->setPosition(Vec2(origin.x + visibleSize.width * 0.2f + (visibleSize.width/2), origin.y + visibleSize.height*0.4f));
+	daughterImage->setPosition(Vec2(origin.x, origin.y + visibleSize.height*0.5f));
 	daughterImage->setScale(1.0f);
+	//daughterImage->setAnchorPoint(ccp(0, 0.5f));
+	
+	enemyImage->setPosition(Vec2(origin.x + visibleSize.width*0.5f, origin.y + visibleSize.height*0.5f));
+	enemyImage->setScale(4.0f);
+	enemyImage->setVisible(true);
 
 	// add the sprite as a child to this layer
 	this->addChild(daughterImage, 0);
+	this->addChild(enemyImage, 1);
+
+	
+	auto enemyMove = MoveTo::create(4, ccp(origin.x, origin.y + visibleSize.height*0.5f));
+	auto enemyRelocate = MoveTo::create(0, ccp(origin.x + visibleSize.width*0.5f, origin.y + visibleSize.height*0.5f));
+
+	auto seq = CCSequence::create(enemyMove, CCDelayTime::create(2), enemyRelocate, CCDelayTime::create(2), NULL);
+	auto rep = CCRepeatForever::create(seq); //무한반복
+	enemyImage->runAction(rep);
+
+	
+	
 }
