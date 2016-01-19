0124450b77d99bd9b351f3c544690892e9b6b6f5
Redrock <leset912@gmail.com>
2016-01-19 14:36:26 -0800

    T23 현재 모든 버튼 다 작동합니다. 아직 각 버튼에 대한 로직은 따로 없는 상태입니다. 혹시 이렇게 일일이 하는 방법 말고 더 좋은 방법이 있다면 가르쳐 주세요.

--- VESS/Classes/UpgradeLayer.h
+++ VESS/Classes/UpgradeLayer.h
@@ -9,19 +9,17 @@ USING_NS_CC;
 class UpgradeLayer : public Layer
 {
 private:
-	/*virtual bool onTouchBegan(cocos2d::Touch* touch, cocos2d::Event* unused_event);
+	virtual bool onTouchBegan(cocos2d::Touch* touch, cocos2d::Event* unused_event);
 	virtual void onTouchMoved(cocos2d::Touch* touch, cocos2d::Event* unused_event);
 	virtual void onTouchCancelled(cocos2d::Touch* touch, cocos2d::Event* unused_event);
 	virtual void onTouchEnded(cocos2d::Touch* touch, cocos2d::Event* unused_event);
 
 	void setTouchListener();
-*/
+
 	char attribute;
 public :
-	static cocos2d::Layer* createLayer();
 	virtual bool init();
 	CREATE_FUNC(UpgradeLayer);
-	bool onTouchBegan(cocos2d::Touch* touch_, cocos2d::Event* event_);
 	
 
 };
--- VESS/Classes/UpgraderLayer.cpp
+++ VESS/Classes/UpgraderLayer.cpp
@@ -1,5 +1,6 @@
 #pragma once
 #include "UpgradeLayer.h"
+#define tag_number 50
 
 bool UpgradeLayer::init()
 {
@@ -20,6 +21,8 @@ bool UpgradeLayer::init()
 	Director::getInstance()->getEventDispatcher()->addEventListenerWithSceneGraphPriority(listener, this);
 	// 강화 샘플 이미지
 	
+
+	
 	auto smith_image = Sprite::create("Images/smith_image.png");
 	auto fire_image = Sprite::create("Images/fire_button.png");
 	auto grass_image = Sprite::create("Images/grass_button.png");
@@ -27,8 +30,9 @@ bool UpgradeLayer::init()
 	auto smelting_image = Sprite::create("Images/smelting_button.png");
 	auto hammering_image = Sprite::create("Images/hammering_button.png");
 	auto quenching_image = Sprite::create("Images/quenching_button.png");
-	auto shop_image = Sprite::create("Images/shop_button.png");
+	auto list_image = Sprite::create("Images/list_button.png");
 	auto upgrade_image = Sprite::create("Images/upgrade_button.png");
+	auto repair_image = Sprite::create("images/repair_button.png");
 	// position the sprite on the center of the screen
 	smith_image->setPosition(Vec2(origin.x + visibleSize.width * 0.2f, origin.y + visibleSize.height*0.4f));
 	smith_image->setScale(1.0f);
@@ -51,11 +55,25 @@ bool UpgradeLayer::init()
 	quenching_image->setPosition(Vec2(origin.x + visibleSize.width * 0.3f, origin.y + visibleSize.height*0.4f));
 	quenching_image->setScale(1.0f);
 
-	shop_image->setPosition(Vec2(origin.x + visibleSize.width * 0.35f, origin.y + visibleSize.height*0.8f));
-	shop_image->setScale(1.0f);
+	list_image->setPosition(Vec2(origin.x + visibleSize.width * 0.35f, origin.y + visibleSize.height*0.8f));
+	list_image->setScale(1.0f);
 
 	upgrade_image->setPosition(Vec2(origin.x + visibleSize.width * 0.05f, origin.y + visibleSize.height*0.8f));
 	upgrade_image->setScale(1.0f);
+
+	repair_image->setPosition(Vec2(origin.x + visibleSize.width * 0.2f, origin.y + visibleSize.height*0.8f));
+	repair_image->setScale(1.0f);
+
+
+	fire_image->setTag(tag_number);
+	grass_image->setTag(tag_number+1);
+	water_image->setTag(tag_number+2);
+	smelting_image->setTag(tag_number+3);
+	hammering_image->setTag(tag_number+4);
+	quenching_image->setTag(tag_number+5);
+	list_image->setTag(tag_number+6);
+	upgrade_image->setTag(tag_number+7);
+	repair_image->setTag(tag_number + 8);
 	// add the sprite as a child to this layer
 	this->addChild(smith_image);
 	this->addChild(fire_image);
@@ -64,8 +82,12 @@ bool UpgradeLayer::init()
 	this->addChild(smelting_image);
 	this->addChild(hammering_image);
 	this->addChild(quenching_image);
-	this->addChild(shop_image);
+	this->addChild(list_image);
 	this->addChild(upgrade_image);
+	this->addChild(repair_image);
+
+
+	
 	// 불속성 이미지
 
 	//if (location_ != NULL) {
@@ -81,61 +103,149 @@ bool UpgradeLayer::init()
 
 bool UpgradeLayer::onTouchBegan(Touch* touch_, Event* event_)
 {
-	Vec2 location = touch_->getLocation();
-	Sprite* spr = (Sprite*) this->getChildByTag(1);
-	Rect rect = spr->getBoundingBox();
 
-	if (rect.containsPoint(location)) {
+	// 0, 1, 2  속성이미지
+	Point p = touch_->getLocation();
+	auto fire_image = (Sprite*) this->getChildByTag(tag_number);
+	Rect rect = fire_image->getBoundingBox();
+	if (rect.containsPoint(p)) {
 
-		spr->setScale(2.0);
+		fire_image->setScale(2.0);
 
 	}
 	else {
 
-		spr->setScale(1);
+		fire_image->setScale(1);
 	}
 
+	auto grass_image = (Sprite*) this->getChildByTag(tag_number+1);
+	Rect rect1 = grass_image->getBoundingBox();
+	if (rect1.containsPoint(p)) {
+
+		grass_image->setScale(2.0);
+
+	}
+	else {
+
+		grass_image->setScale(1);
+	}
+
+	auto water_image = (Sprite*) this->getChildByTag(tag_number + 2);
+	Rect rect2 = water_image->getBoundingBox();
+	if (rect2.containsPoint(p)) {
+
+		water_image->setScale(2.0);
+
+	}
+	else {
+
+		water_image->setScale(1);
+	}
+
+
+	// 3, 4, 5  강화이미지
+	auto smelting = (Sprite*) this->getChildByTag(tag_number + 3);
+	Rect rect3 = smelting->getBoundingBox();
+	if (rect3.containsPoint(p)) {
+
+		smelting->setScale(2.0);
+
+	}
+	else {
+
+		smelting->setScale(1);
+	}
+
+	auto hammering = (Sprite*) this->getChildByTag(tag_number + 4);
+	Rect rect4 = hammering->getBoundingBox();
+	if (rect4.containsPoint(p)) {
+
+		hammering->setScale(2.0);
+
+	}
+	else {
+
+		hammering->setScale(1);
+	}
+
+	auto quenching = (Sprite*) this->getChildByTag(tag_number + 5);
+	Rect rect5 = quenching->getBoundingBox();
+	if (rect5.containsPoint(p)) {
+
+		quenching->setScale(2.0);
+
+	}
+	else {
+
+		quenching->setScale(1);
+	}
+
+	//6, 7, 8 강화,도감,수리
+	auto upgrade = (Sprite*) this->getChildByTag(tag_number + 6);
+	Rect rect6 = upgrade->getBoundingBox();
+	if (rect6.containsPoint(p)) {
+
+		upgrade->setScale(2.0);
+
+	}
+	else {
+
+		upgrade->setScale(1);
+	}
+
+	auto list = (Sprite*) this->getChildByTag(tag_number + 7);
+	Rect rect7 = list->getBoundingBox();
+	if (rect7.containsPoint(p)) {
+
+		list->setScale(2.0);
+
+	}
+	else {
+
+		list->setScale(1);
+	}
+
+	auto repair = (Sprite*) this->getChildByTag(tag_number + 8);
+	Rect rect8 = repair->getBoundingBox();
+	if (rect8.containsPoint(p)) {
+
+		repair->setScale(2.0);
+
+	}
+	else {
+
+		repair->setScale(1);
+	}
 	return true;
+
 }
 
 
-//void UpgradeLayer::setTouchListener()
-//{
-//	// make touch listener
-//
-//	auto listener = EventListenerTouchOneByOne::create();
-//	listener->onTouchBegan = CC_CALLBACK_2(UpgradeLayer::onTouchBegan, this);
-//	listener->onTouchMoved = CC_CALLBACK_2(UpgradeLayer::onTouchMoved, this);
-//	listener->onTouchCancelled = CC_CALLBACK_2(UpgradeLayer::onTouchCancelled, this);
-//	listener->onTouchEnded = CC_CALLBACK_2(UpgradeLayer::onTouchEnded, this);
-//	_eventDispatcher->addEventListenerWithSceneGraphPriority(listener, this);
-//}
-//
-//bool UpgradeLayer::onTouchBegan(cocos2d::Touch* touch, cocos2d::Event* unused_event)
-//{
-//	
-//	return true;
-//}
-//
-//void UpgradeLayer::onTouchMoved(cocos2d::Touch* touch, cocos2d::Event* unused_event)
-//{
-//
-//}
-//
-//void UpgradeLayer::onTouchCancelled(cocos2d::Touch* touch, cocos2d::Event* unused_event)
-//{
-//
-//}
-//
-//void UpgradeLayer::onTouchEnded(cocos2d::Touch* touch, cocos2d::Event* unused_event)
-//{
-//	auto target = this->getChildByName("grass_image");
-//	if (target == NULL) 
-//	{
-//		log("fxxk");
-//	}else if (target->boundingBox().containsPoint(touch->getLocation()))
-//	{
-//		log("grasssssss");
-//	}
-//}
+void UpgradeLayer::setTouchListener()
+{
+	// make touch listener
+
+	auto listener = EventListenerTouchOneByOne::create();
+	listener->onTouchBegan = CC_CALLBACK_2(UpgradeLayer::onTouchBegan, this);
+	listener->onTouchMoved = CC_CALLBACK_2(UpgradeLayer::onTouchMoved, this);
+	listener->onTouchCancelled = CC_CALLBACK_2(UpgradeLayer::onTouchCancelled, this);
+	listener->onTouchEnded = CC_CALLBACK_2(UpgradeLayer::onTouchEnded, this);
+	_eventDispatcher->addEventListenerWithSceneGraphPriority(listener, this);
+}
+
+
+void UpgradeLayer::onTouchMoved(cocos2d::Touch* touch, cocos2d::Event* unused_event)
+{
+
+}
+
+void UpgradeLayer::onTouchCancelled(cocos2d::Touch* touch, cocos2d::Event* unused_event)
+{
+
+}
+
+void UpgradeLayer::onTouchEnded(cocos2d::Touch* touch, cocos2d::Event* unused_event)
+{
+
+}
 
