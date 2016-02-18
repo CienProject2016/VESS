c8be3d3723565412a6aabe238f9963a04d0afd93
minseok1 <paran2008@naver.com>
2016-02-18 12:38:44 +0900

    튜토리얼 레이어 생성

---
+++ VESS/Classes/TutorialLayer.h
@@ -0,0 +1,31 @@
+#pragma once
+#ifndef __TutorialLayer_H_
+#define __TutorialLayer_H_
+#include "cocos2d.h"
+#include "WindowSize.h"
+#include <vector>
+#include "GameData.h"
+#include "GameScene.h"
+
+USING_NS_CC;
+
+class TutorialLayer : public Layer
+{
+	private:
+		virtual bool onTouchBegan(cocos2d::Touch* touch, cocos2d::Event* event_);
+		virtual void onTouchMoved(cocos2d::Touch* touch, cocos2d::Event* unused_event);
+		virtual void onTouchCancelled(cocos2d::Touch* touch, cocos2d::Event* unused_event);
+		virtual void onTouchEnded(cocos2d::Touch* touch, cocos2d::Event* unused_event);
+
+		//vector<Tutorial> *tutorialData;
+		//vector<Tutorial>::iterator tutorialIterator;
+
+		void setTouchListener();
+
+	public:
+		bool init();
+		//bool hasNextTutorial();
+		//void showNextTutorial();
+		CREATE_FUNC(TutorialLayer);
+};
+#endif
